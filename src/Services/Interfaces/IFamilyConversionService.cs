c2dc46446ffa129c8ad0fb3122f160ae06653be7
luiscortes-arq <luis.cortes@hermosillo.com>
2026-02-21 01:37:53 -0600

    feat(phase-18): implement batch conversion and in-place replacement logic

--- src/Services/Interfaces/IFamilyConversionService.cs
+++ src/Services/Interfaces/IFamilyConversionService.cs
@@ -1,11 +1,12 @@
 using Autodesk.Revit.DB;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace LECG.Services.Interfaces
 {
     public interface IFamilyConversionService
     {
         void ConvertFamily(Document doc, FamilyInstance instance, string name, string templatePath, bool isTemporary);
+        void ConvertFamilyBatch(Document doc, IEnumerable<FamilyInstance> instances, string customName, string templatePath, bool isTemporary, bool replaceInPlace);
         string GetTargetTemplatePath(Autodesk.Revit.ApplicationServices.Application app, Category category);
     }
 }
